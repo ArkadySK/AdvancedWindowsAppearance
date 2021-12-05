975f081f402e35e11db71e4de7ddc7f33841ba91
ArkadySK <adamkooo2@gmail.com>
2021-12-05 14:20:09 +0100

    added restore window

---
+++ RestoreWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdvancedWindowsAppearence
+{
+    /// <summary>
+    /// Interaction logic for RestoreWindow.xaml
+    /// </summary>
+    public partial class RestoreWindow : Window
+    {
+        public RestoreWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();    
+        }
+    }
+}
