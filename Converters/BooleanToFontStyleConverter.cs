cf5c66767b5cb20db06038317d52ee4077869db9
ArkadySK <adamkooo2@gmail.com>
2021-12-22 00:19:59 +0100

    added converters for font bindings

--- Converters/BooleanToFontStyleConverter.cs
+++ Converters/BooleanToFontStyleConverter.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Drawing;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace AdvancedWindowsAppearence.Converters
 {
-    internal class FontToFontFamilyConverter : IValueConverter
+    internal class BooleanToFontStyleConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            Font f = (Font)value;
-            return f.Name;
+            bool isItalic = (bool)value;
+            if (isItalic)
+                return FontStyles.Italic;
+            else
+                return FontStyles.Normal;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- Converters/BooleanToFontWeightConverter.cs
+++ Converters/BooleanToFontWeightConverter.cs
@@ -1,16 +1,19 @@
 ﻿using System;
-using System.Drawing;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace AdvancedWindowsAppearence.Converters
 {
-    internal class FontToFontFamilyConverter : IValueConverter
+    internal class BooleanToFontWeightConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            Font f = (Font)value;
-            return f.Name;
+            bool isBold = (bool)value;
+            if (isBold)
+                return FontWeights.Bold;
+            else
+                return FontWeights.Regular;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
