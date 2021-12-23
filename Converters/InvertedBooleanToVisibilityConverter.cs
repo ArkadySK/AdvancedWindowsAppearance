bc46bbe2593f4b9b48983c63254a839c153ffded
ArkadySK <adamkooo2@gmail.com>
2021-12-23 12:46:50 +0100

    fixed crash on start on win 8

--- Converters/InvertedBooleanToVisibilityConverter.cs
+++ Converters/InvertedBooleanToVisibilityConverter.cs
@@ -13,6 +13,7 @@ namespace AdvancedWindowsAppearence.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (value == null) return Visibility.Collapsed;
             if ((bool)value)
                 return Visibility.Collapsed;
             else
