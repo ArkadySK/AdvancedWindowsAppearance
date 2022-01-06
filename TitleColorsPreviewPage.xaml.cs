6f4760a1311f0f39bdf1bf361709f864139edd96
ArkadySK <adamkooo2@gmail.com>
2022-01-06 22:25:31 +0100

    added preview for aero colors

---
+++ TitleColorsPreviewPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdvancedWindowsAppearence
+{
+    /// <summary>
+    /// Interaction logic for TitleColorsPreviewPage.xaml
+    /// </summary>
+    public partial class TitleColorsPreviewPage : Page
+    {
+        public TitleColorsPreviewPage()
+        {
+            InitializeComponent();
+            this.DataContext = Application.Current.MainWindow.DataContext;
+        }
+    }
+}
