caa42f7861061c9aec6c9339d010382fb9726236
ArkadySK <adamkooo2@gmail.com>
2022-03-01 15:28:13 +0100

    added proper gradient to old buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -66,7 +66,16 @@ namespace AdvancedWindowsAppearence
         {
             if (WinVer < new Version(6, 2))
             {
-                LinearGradientBrush buttonBrush = new LinearGradientBrush(System.Windows.SystemColors.ControlColor, System.Windows.SystemColors.ControlLightColor, 90);
+                //LinearGradientBrush buttonBrush = new LinearGradientBrush(System.Windows.SystemColors.ControlColor, System.Windows.SystemColors.ControlLightColor, 90);
+                LinearGradientBrush buttonBrush = new LinearGradientBrush(
+                    new GradientStopCollection(
+                        new GradientStop[]
+                        {
+                            new GradientStop(System.Windows.SystemColors.ControlColor, 0.5),
+                            new GradientStop(System.Windows.SystemColors.ControlLightColor, 0.5)
+                        }),
+                    90
+                    );
                 App.Current.Resources["ButtonFaceColor"] = buttonBrush;
             }
 
