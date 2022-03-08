c410836667ee662901e0d29c795119e7b73a7917
ArkadySK <adamkooo2@gmail.com>
2022-03-08 17:16:34 +0100

    added color modes to the theme settings

---
+++ Converters/BooleanToIntConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AdvancedWindowsAppearence.Converters
+{
+    public class BooleanToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) 
+                return 0;
+
+            if(value is bool boolValue)
+                return boolValue ? 1 : 0;
+            return 0;
+                
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return false;
+            if (value is int intValue)
+                if (intValue == 0)
+                    return false;
+            return true;
+        }
+    }
+}
