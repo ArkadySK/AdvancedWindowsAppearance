0db847f84c22d1ed1c05f2dc8268ffad8ca827e6
ArkadySK <adamkooo2@gmail.com>
2022-04-08 20:14:25 +0200

    added enum to string converter

---
+++ Converters/EnumToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AdvancedWindowsAppearence.Converters
+{
+    internal class EnumToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string EnumString;
+            try
+            {
+                EnumString = Enum.GetName((value.GetType()), value);
+                return EnumString;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
