f99c8cbebae8d858bc48727124c4206e4828587e
ArkadySK <adamkooo2@gmail.com>
2022-04-26 16:44:50 +0200

    added path convertion methods into Win32Methods class

--- Win32Methods.cs
+++ Win32Methods.cs
@@ -9,9 +9,11 @@ namespace WindowsInteropServices
 {
     internal class Win32Methods
     {
+        
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 
+        // The method to update desktop wallpaper in this case
         public static void UpdateWallpaper(string path)
         {
             const int SPI_SETDESKWALLPAPER = 20;
@@ -23,5 +25,24 @@ namespace WindowsInteropServices
             path,
             SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
         }
+
+
+        /// <summary>
+        /// converts an item identifier list to a file system path
+        /// </summary>
+        /// <param name="pidl">the address of the path</param>
+        /// <param name="pszPath">the output string it will save into</param>
+        /// <returns></returns>
+
+        [DllImport("shell32.dll")]
+        public static extern Int32 SHGetPathFromIDList(
+            IntPtr pidl,
+            StringBuilder pszPath
+        );
+
+
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr ILCreateFromPath([In, MarshalAs(UnmanagedType.LPWStr)] string pszPath);
+
     }
 }
