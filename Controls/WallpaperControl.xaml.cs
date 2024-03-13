8eed8097b2cc931a6ef23bab02af182e985a2d96
adam majerik <adamkooo2@gmail.com>
2024-03-13 19:51:41 +0100

    fix for wallpaper preview issue

--- Controls/WallpaperControl.xaml.cs
+++ Controls/WallpaperControl.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AdvancedWindowsAppearence.Controls
 {
@@ -25,9 +13,9 @@ namespace AdvancedWindowsAppearence.Controls
             InitializeComponent();
 
             double w = SystemParameters.VirtualScreenWidth;
-            double h = SystemParameters.VirtualScreenHeight;
+            //double h = SystemParameters.VirtualScreenHeight;
             wallpaperGrid.Width = w / 10;
-            wallpaperGrid.Height = h / 10;
+            //wallpaperGrid.Height = h / 10;
         }
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
